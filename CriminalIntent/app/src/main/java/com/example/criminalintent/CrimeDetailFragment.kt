4a52097552fc594d809febc1c0976a32f41bf2ec
ionnomirai <kindbakaneko@gmail.com>
2023-04-05 23:06:36 +0300

    05.04.2023 - Added some features
    - added dependencies for more features for working with Fragment, ViewModels, and Activity
    
    - in activity_main.xml removed the name attribute from the FragmentContainerView.
      Purpose: to leave the connection in only one place, I chose what it will be in the code
    
    - in MainActivity added a check for the existence of a fragment instance in the list of FragmentManager fragments.
      Purpose: Cancel re-adding to the list (resource saving)
    
    - in CrimeDetailFragment added a copy of the binding variable with a null value, its initialization will be carried out later in the "onCreateView" function. When calling the original variable to return root or access an element, the get property will be called referring to the already initialized copy. If it is still null, an exception will be thrown.
      Purpose: Eliminate the link between the fragment and the Binding instance when the fragment is destroyed - this will eliminate the memory leak. When the fragment is recreated, a new binding view will be inflated. To achieve the goal, the function "onDestroyView" was redefined where the binding copy was set to null
    
    - the CrimeListFragment class was created, which will act as a fragment for displaying a list of "crimes"
    - CrimeListViewModel was created - viewModel for CrimeListFragment fragment. It currently contains a list of "crimes" randomly generated.

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.criminalintent
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,17 +12,25 @@ import java.util.*
 
 class CrimeDetailFragment : Fragment(){
     private lateinit var crime: Crime
-    private lateinit var binding: FragmentCrimeBinding // instance of the layout
+
+    /* */
+    private var _binding: FragmentCrimeBinding? = null //instance of the layout
+    //checkNotNull(value) if value equals null -> than throe exception, otherwise return value
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot accesss binding because it is null. Is the view visible?"
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d("Fragment", "onCreateFragment")
         crime = Crime(
             id = UUID.randomUUID(),
             title = "",
             date = Date(),
             isSolved = false
         )
+        Log.d("Fragment", "Crime id: ${crime.id}")
     }
 
     //in that function we create instance of the layout and poss it to the host
@@ -30,17 +39,18 @@ class CrimeDetailFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("Fragment", "onCreateView")
         //layoutInflater it is equals getLayoutInflater
         //question! Can we call inflater instead getLayoutInflater?
         //purpose: create an instance of the layout
-        binding = FragmentCrimeBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentCrimeBinding.inflate(layoutInflater, container, false)
         return binding.root //go to the root element of views that contains all other views
     }
 
     //in that function we initialise variables and setting listeners
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        Log.d("Fragment", "onViewCreated")
         binding.apply {
             /*it is filling the title field in crime object, at the same time that the user enters
             this information into EditText* field*/
@@ -62,5 +72,11 @@ class CrimeDetailFragment : Fragment(){
             }
         }
 
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        Log.d("Fragment", "onDestroyView")
     }
 }
--- CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -8,11 +8,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //if fragment exist in the list of fragment, than do nothing, otherwise create and add fragment
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if(currentFragment == null){
             val fragment = CrimeDetailFragment()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
                 .commit()
-
+        }
     }
 }
