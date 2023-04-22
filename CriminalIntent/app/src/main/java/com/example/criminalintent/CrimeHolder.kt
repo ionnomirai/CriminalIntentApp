d81dc9402f54d4a2df349453e017cf6e32eab4af
ionnomirai <kindbakaneko@gmail.com>
2023-04-22 23:15:14 +0300

    22.04.2023 Added some features
    * Added vector image of handcuffs;
    * Changed list_item_crime from LinearLayout to ConstraintLayout;
    * Settings on list_item_crime layout are set:
    	- set limits on both textView from layout list_item_crime;
    	- set ?attr/textAppearanceHeadline6 style for tv_crime_title field
    	- added vector image of handcuffs to the right side of the screen;
    * Added logic for displaying handcuffs on the screen in CrimeHolder. If the crime is solved crime.isSolved==true --> then display the image, otherwise do not display;
    * Converted the date output in the CrimeHolder class to the tvCrimeData field. For now, the output will be like: "Wednesday, May 11, 2022"

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeHolder.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeHolder.kt
@@ -1,8 +1,11 @@
 package com.example.criminalintent
 
+import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.criminalintent.databinding.ListItemCrimeBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 //simply put, this is like model of item
 class CrimeHolder(
@@ -10,8 +13,18 @@ class CrimeHolder(
 ) : RecyclerView.ViewHolder(bindingMy.root) {
     fun bind(crime: Crime) {
         bindingMy.apply {
+            //Task 22.04.23_1: show date like: "Wednesday, May 11, 2022"
+            val formatter = SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.getDefault())
+            val answer: String = formatter.format(crime.date)
+
             tvCrimeTitle.setText(crime.title)
-            tvCrimeData.setText(crime.date.toString())
+            tvCrimeData.setText(answer)
+
+            crimeSolved.visibility = if(crime.isSolved){
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
 
             root.setOnClickListener {
                 Toast.makeText(root.context, "${crime.title} clicked!", Toast.LENGTH_SHORT).show()
@@ -19,3 +32,4 @@ class CrimeHolder(
         }
     }
 }
+
